8dc459529f152cc869ba5c4b747109a7c2b2e2a2
LeoDr99 <github@phod.eu>
2020-10-08 14:13:52 +0000

    Added alert monitoring

--- distributedIndustryMonitor/controller.lua
+++ distributedIndustryMonitor/controller.lua
@@ -1,5 +1,5 @@
 --[[ --------MASTER/CONTROLLER Prog. Board
-     Insert code onto unit>>start() filter and the timer/tick. 
+     Setup:
 
      Connect/link the databank that is connected to the worker/slave boards, connect the core (don't forget this, or no custom names shall show up!),
      and displays (connect one display per worker/slave board that you'll use/define; and NAME THEM! or use the default slot names, but remmember who is who ffs, you'll need it later).
@@ -13,7 +13,7 @@
      then you'll need 3 displays. each display is going to show you their respective board (1:1 ratio for now; working on this part).
      Now, how do you do that, simple. To try and keep it simple for even new players to the game, I tried to keep this realy basic. so, here we go.
 
-     duplicate as many times, as many displays/worker boards you've got connected, the following line that you have put into the tick/timer section "screen1":
+     duplicate as many times, as many displays/worker boards you've got connected, the following line that you have put into the tick/timer section "screens":
      display.setHTML(renderHTML("PB1"))
 
      If you notice the PB1, is the default unique ID for the initial single worker/slave board. and you'll also notice that the display slot is named "display".
@@ -26,23 +26,39 @@
      |----------------|                   |----------|
      display_slot_name  . function ( function ('UID you defined on the worker/slave board') )
 
-     The you have it.
+     There you have it.
 
      So, just make sure, that you are using unique identifiers on the worker/slave boards (check the worker.lua header for info), and that you know where the connected displays are linked to (which slots on the controller board).
      Then, put an extra line on the tick/timer filter, with the correct display slot name, and the worker/slave board ID, and you shoul'd be golden.
 
+     Insert code below onto unit>>start() filter and the timer/tick.
      ]]
 --// the "magic" starts here...
 --
-if display then
-    display.activate()
-    display.setCenteredText("Industry Monitor Starting...")
+if display1 then
+    display1.activate()
+    display1.setCenteredText("Industry Monitor Starting...")
     end
+if display2 then
+    display2.activate()
+    display2.setCenteredText("Industry Monitor Starting...")
+    end
+if display3 then
+    display3.activate()
+    display3.setCenteredText("Industry Monitor Starting...")
+    end
+if displayAlerts then
+    displayAlerts.activate()
+    displayAlerts.setCenteredText("Industry Monitor Starting...")
+    end
+
+
 
 unit.hide()
 --
 
 --variables
+_mode = 0 --export: default mode: Industry Monitor (0) alternate mode, Alert Monitor (1), the latter will only exhibit error'd industries.
 local refresh = 10 --export: refresh info screen every # seconds (keep it higher than the worker boards)
 local _strON  = "ON" --export: string to display when industry is ON
 local _strOFF = "OFF" --export: string to display when industry is OFF
@@ -51,8 +67,8 @@ local _strREZ = "REZ" --export: string to display when industry is in need for r
 local _strERR = "ERR" --export: string to display when industry is in fault/error mode
 local _colorBGHeader = "red" --export: html color code, that you want the header background to have. can be #ffffff values, or simple colour names (red, yellow, green.... check htmlcolorcodes.com ;] ) 
 local _colorFGHeader = "black" --export: html colour code, that you want the header font to have. can be #ffffff values, or simple colour names (red, yellow, green.... check htmlcolorcodes.com ;] ) 
-local _colorFGHeader = "black" --export: html colour code, that you want the header font to have. can be #ffffff values, or simple colour names (red, yellow, green.... check htmlcolorcodes.com ;] ) 
 local _colorTable = "white" --export: industry names and efficiency font colour
+_dbCounter = 0
 --
 
 --functions
@@ -62,7 +78,41 @@ function tablelenght(input)
     return count
 end
 --
+function getAlerts()
+    local _keys = json.decode(databank.getKeys())
+    local _datum = {}
+    local strings = {}
 
+    if _keys then
+        for i=1, #_keys do
+            local _tmp = json.decode(databank.getStringValue(_keys[i]))
+            for x=1, tablelenght(_tmp) do
+                if string.match(_tmp[x].status, "ERR") then
+                    table.insert(_datum, core.getElementNameById(_tmp[x].id))
+                end
+            end
+        end
+    else
+        system.print("MASTER:DB_ERR:NO_KEYS! Empty Keys!")
+    end
+    if #_datum > 0 then
+        local count = 0
+        for z=1, tablelenght(_datum) do
+            local str = ""
+            count = count + 1
+            str = str .. [[<th style="text-align: left;">]] .. _datum[z] .. [[</th><th style="color: red; !important;">]] .. _strERR .. "</th>"
+            table.insert(strings, str)
+            if count == 9 then
+                break
+            end
+        end
+        return strings
+    else
+        table.insert(strings, [[<td colspan="2" style="text-align: center; color: green; !important; max-width: 100%; font-size: 12rem;">!ALL CLEAR!</td>]])
+        return strings
+    end
+end
+--
 function getWorkerDbData(_PBID)
     local _data = json.decode(databank.getStringValue(_PBID))
     local strings = {}
@@ -72,67 +122,123 @@ function getWorkerDbData(_PBID)
             if string.match(_data[k].status, "OFF") then 
                     str = str .. [[<th style="text-align: left;">]] .. core.getElementNameById(_data[k].id) .. "</th><th>" .. _strOFF .. "</th><th>NaN</th>"
                 elseif string.match(_data[k].status, "ON") then 
-                    str = str .. [[<th style="text-align: left;">]] .. core.getElementNameById(_data[k].id) .. [[</th><th style="color: green;">]] .. _strON .. "</th><th>" .._data[k].efficiency .."</th>"
+                    str = str .. [[<th style="text-align: left;">]] .. core.getElementNameById(_data[k].id) .. [[</th><th style="color: green;">]] .. _strON .. "</th><th>" .. string.format("%.1f",_data[k].efficiency * 100) .."</th>"
                 elseif string.match(_data[k].status, "HLD") then 
-                    str = str .. [[<th style="text-align: left;">]] .. core.getElementNameById(_data[k].id) .. [[</th><th style="color: yellow;">]] .. _strHLD .. "</th><th>" .._data[k].efficiency .."</th>"
+                    str = str .. [[<th style="text-align: left;">]] .. core.getElementNameById(_data[k].id) .. [[</th><th style="color: yellow;">]] .. _strHLD .. "</th><th>" .. string.format("%.1f",_data[k].efficiency * 100) .."</th>"
                 elseif string.match(_data[k].status, "REZ") then 
-                    str = str .. [[<th style="text-align: left;">]] .. core.getElementNameById(_data[k].id) .. [[</th><th style="color: orange; !important;">]] .. _strREZ .. "</th><th>" .._data[k].efficiency .."</th>"
+                    str = str .. [[<th style="text-align: left;">]] .. core.getElementNameById(_data[k].id) .. [[</th><th style="color: orange; !important;">]] .. _strREZ .. "</th><th>" .. string.format("%.1f",_data[k].efficiency * 100) .."</th>"
                 elseif string.match(_data[k].status, "ERR") then 
-                    str = str .. [[<th style="text-align: left;">]] .. core.getElementNameById(_data[k].id) .. [[</th><th style="color: red; !important;">]] .. _strERR .. "</th><th>" .._data[k].efficiency .."</th>"
-                else str = [[<th style="color: red; !important;">FUBAR</th><th></th><th></th>]]
+                    str = str .. [[<th style="text-align: left;">]] .. core.getElementNameById(_data[k].id) .. [[</th><th style="color: red; !important;">]] .. _strERR .. "</th><th>" .. string.format("%.1f",_data[k].efficiency * 100) .."</th>"
+                else str = [[<th colspan="3" style="color: red; !important;">FUBAR</th>]]
             end
-            strings[k] = str
+            table.insert(strings, str)
         end
-    else system.print("!!DB READ ERROR!!")  --you forgot to connect the damned databank, or turn on the worker/slave board :P
+    else system.print("DB_WARN:READ_ERROR!")  --you forgot to connect the damned databank, or turn on the worker/slave board :P
     end
     --
     return strings        
 end    
 --
-
 function renderHTML(_pbID)
     local htmlout = ""
-    local _status = getWorkerDbData(_pbID)
+    local _status = {}
+    local header = ""
+    local table = ""
+    local div = ""
+    if _mode == 1 or string.match(_pbID, "lert") then
+        _status = getAlerts()
+        header = "<th>ALERT</th><th>CODE</th>"
+        table = [[<table style="width: 100%; font-size: 8rem;">]]
+        div = [[<div class="row" style="justify-content: center;">]]
+    else
+        _status = getWorkerDbData(_pbID)
+        header = "<th>Industry</th><th>Status</th><th>%</th>"
+        table = [[<table style="margin-left: auto; margin-right: auto; width: 98%; font-size: 9rem;">]]
+        div = [[<div class="row" style="justify-content: center;">]]
+    end
     --
-    for i=1, tablelenght(_status) do
+    for i=1, #_status do
         htmlout = htmlout .. [[<tr style="font-size: 7rem; font-family: roboto">]] .. _status[i] .. "</tr>"
     end
     --
     local html = [[
     <style>
+    body {
+      border: 3px solid red;
+    }
+    
     .container {
       font-size: 10rem;
       color:]] .. _colorTable .. [[;
     }
     </style>
-    <div class="container">
-      <div class="row" style="margin-top: auto; justify-content: center;">
-        <table style="margin-left: auto; margin-right: auto; width: 98%; font-size: 9rem;">
-          <tr style="background-color:]] .. _colorBGHeader .. "; color: "  .. _colorFGHeader .. [[; font-size: 10rem; text-align: center;">
-            <th>Industry</th><th>Status</th><th>%</th>
-          </tr>
-    ]] .. htmlout .. [[</table></div></div>]]
+    <div class="container">]] .. div .. table ..
+    [[<tr style="background-color:]] .. _colorBGHeader ..
+    "; color: "  .. _colorFGHeader .. [[; font-size: 10rem; text-align: center;">]] 
+    .. header ..[[</tr>]] .. htmlout .. [[</table></div></div>]]
     --    
     return html
 end
 --
-
 --runtime
-unit.setTimer("screens", refresh)
-
+if databank then
+    databank.clear()
+    system.print("MASTER: DB_CLEAR!")
+    unit.setTimer("screens", refresh)
+else
+    system.print("ERROR: DB MISSING!")
+end
 
 
 --[[ insert into unit>>tick("screens") filter]]
 --
-display.setHTML(renderHTML("PB1"))
-
+_dbCounter = _dbCounter + 1
+if _dbCounter >= 10 then
+    databank.clear()
+    _dbCounter = 0
+    --this will clear all keys from the DB every 10 cycles (10 x refresh timer; seconds)
+    --in order to display properly, if you remove/add new industries, during runtime.
+else
+    display1.setHTML(renderHTML('PB1'))
+    display2.setHTML(renderHTML('PB2'))
+    display3.setHTML(renderHTML('PB3'))
+    displayAlerts.setHTML(renderHTML('Alerts'))
+    --[[insert or remove more above this line, one line per display:pboard combo, example:
+    display2.setHTML(renderHTML('PB2') -- it'll show the status of the industries connected to
+    worker prog.broad with UID "PB2" on the display connected to, the named slot "display2".
+    
+    Notes: 
+    1)if running in Alert Monitor Mode (only), you'll need only one display. so just add/leave a line like this, or leave the default one:
+    display.setHTML(renderHTML('Alerts') -- make sure there's a display connected and that its slot
+    is named "display". if not, change the line accordingly. also ensure that the lua parameter for the mode is set to 1.
+    In this mode, it'll show only industries connected to the worker prog.boards that are in error (failed status; check codex for them.).
+    
+    2) dual mode (monitor + alerts)
+    then follow the initial instruction (not note #1), and create another line like this:
+    displayAlerts.setHTML(renderHTML('Alerts')) -- where displayAlerts is the name of the slot where you have the dedicated monitor connected.
+    you can have displays to monitor the industry and a added one just for Alerts/Alarms (faulted industries; be advised that it's not yet possible
+     identify industries with Unknown Server Error. since those do not provide any alarm code via the lua API.).
+    ]]
+end
 
 
 --[[ insert into unit>>stop() filter]]
 --
-if display then
-    display.clear()
-    display.deactivate()
+if display1 then
+    display1.clear()
+    display1.deactivate()
+    end
+if display2 then
+    display2.clear()
+    display2.deactivate()
+    end
+if display3 then
+    display3.clear()
+    display3.deactivate()
+    end
+if displayAlerts then
+    displayAlerts.clear()
+    displayAlerts.deactivate()
     end
 
 --EOF
--- distributedIndustryMonitor/worker.lua
+++ distributedIndustryMonitor/worker.lua
@@ -1,21 +1,18 @@
 --[[ -------------------WORKER Prog. Board
-     Insert code onto unit>>start() filter and the timer/tick (check the comments, it tells you everything), connect the databank, and name the slot databank.
-     then connect/link your industries that you want to monitor, on the remaining 9 slots.
-     after all links are setup, rename the connected industries, with discernable names, doesn't matter what, anything goes.
-     Next, edit the lua parameters, right click the PBoard, choose advanced>>edit lua parameters.
-     please make sure that the Programming board ID is unique, as not in use by any other worker/slave board that you'll connect
-     to the databank. Also choose a refresh rate (timer for the db sync), make sure that it's less than the controller refresh timer.
-     also, small tip, to not use a lot of cpu resources, i recommend to keep at >=5 seconds. it all depends on the performance of
-     your rig, and how fast you want to see the industry status changing... are you that bored? :D
-     ]]
+     setup: make sure a databank is connected and named as such.
+     then just edit the lua parameters (right click, advanced>edit lua parameters)
+     connect the industries, up to 9 of them.
+     rename your industries, in order to show up on the screens properly.
+     --
+     Insert code below onto unit>>start() filter]]
 --
 unit.hide()
 --helpers
 json = require ("dkjson")
 
 --variables
-local PBID = "PB1" --export: this worker's unique ID (Ex:. PB1; for databank purposes)
-local refresh = 5 --export: refresh interval (write to db, every # seconds; keep it lower than the master/controller refresh)
+PBID = "PB1" --export: this worker's board unique ID (Ex:. PB1; for databank purposes; must be unique!)
+local refresh = 7 --export: refresh interval (write to db, every # seconds; keep it lower than the master controller refresh timer. recommended >=15s for reduced cpu load)
 
 --functions
 function tablelenght(input)
@@ -36,7 +33,7 @@ function updateIndustry(_PBID)
         elseif string.match(_status, "PENDING") then
             result = "HLD"
         elseif string.match(_status, "INGREDIENT") then
-            result = "REZ "
+            result = "REZ"
         else
             result = "ERR"
             end
@@ -54,8 +51,6 @@ function updateIndustry(_PBID)
     end
        
     --//----------//--
-    --runtime
-    
     for key, value in pairs(unit) do
         if type(value) == "table" and type(value.export) == "table" then -- `value` is an element and `key` is the slot name
             if value.getElementClass then
@@ -80,8 +75,13 @@ end
 unit.setTimer("updateDB", refresh)
 
 
---[[ inserto into unit>>tick(updateDB) filter]]
+
+--[[ inserto code below onto unit>>tick(updateDB) filter]]
 --
-writeDB()
+if databank then
+    writeDB()
+else
+    system.print("DB_ERR: NO DATABANK FOUND! check if databank slot is named as such!")
+end
 
 --EOF
